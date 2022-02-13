76622b7493903b8cf325b6c98a0984bd88b3e17c
DeepaMadheswaran <99516695+DeepaMadheswaran@users.noreply.github.com>
2022-02-13 10:42:18 +0530

    Delete tomcat_4.sh

--- linux/tomcat_4.sh
+++
@@ -1,9 +0,0 @@
-# !bin/bash
-
-#To find all file ends with .java
-find /home/deepa/dir1/*.java -mtime -10
-
-rm -r $(find /home/deepa/dir1/*.java -mtime -10)
-
-find /home/deepa/dir1/*.java -mtime -10
-
